1fdc1dcaff6805fdab9a7489ec524d86a0b2ec4f
Jairo B <jairock@outlook.com>
2024-08-11 17:27:20 -0600

    feat(server): configure server with routes, use cases, CORS, and Helmet
    - Set up server with route handlers and use cases integration
    - Configured CORS for handling cross-origin requests
    - Integrated Helmet for security headers

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/post", postRoutes);
+
+app.get("/", (request, response) => {
+    response.json({
+        success: true,
+        message: "API bcknd"
+    });
+});
+
+module.exports = app;
