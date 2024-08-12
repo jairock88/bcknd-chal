54a1467086c8a62c41d5727d25db8cd5c858970c
Jairo B <jairock@outlook.com>
2024-08-11 18:04:54 -0600

    feat(user): implement login and signup use cases
    - Created use cases for user login and signup
    - Tested user creation with Thunder Client successfully

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -27,9 +27,47 @@ router.post("/login", async (request, response) => {
   
 
 // Signup route localhost:port/signup
-router.post('/signup', (request, response) => {})
+router.post('/signup', async (request, response) => {
+  try {
+    const userData = request.body;
+    const newUser = await userUseCases.signUp(userData);
+
+    response.json({
+      success: true,
+      message: 'User created',
+      data: { user: newUser },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // Get user information for Id
-router.get('/:id', async (request, response) => {})
+router.get('/:id', async (request, response) => {
+  try {
+    const id = request.params.id;
+    const user = await userUseCases.getUserById(id);
+
+    if (!user) {
+      throw createHttpError(404, 'User not found');
+    }
+    response.json({
+      success: true,
+      message: 'User found',
+      data: { user },
+    });
+
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
