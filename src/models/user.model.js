d45455af0c7d4946d4dcdccac1ce347037215318
Jairo B <jairock@outlook.com>
2024-09-14 23:30:53 -0600

    update signUp schema (include userName)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,28 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: 'string',
-        required: true,
-        maxlength: 120,
-        minlength: 2
+      type: "string",
+      required: true,
+      maxlength: 120,
+      minlength: 2,
+    },
+    userName: {
+      type: "string",
+      required: true,
+      maxlength: 120,
+      minlength: 2,
     },
     profilePic: {
-        type: 'string',
-        default: "https://randomuser.me/api/portraits/lego/6.jpg"
+      type: "string",
+      default: "https://randomuser.me/api/portraits/lego/6.jpg",
     },
-    email:{
-        type: 'string',
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
+    email: {
+      type: "string",
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
-        type: 'string',
-        required: true,
-        minlength: 6
-    }
-},{timestamps: true});
+      type: "string",
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
