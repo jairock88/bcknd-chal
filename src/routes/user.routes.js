120c0aad8a141db5197ee38118604ad1db307448
Jairo B <jairock@outlook.com>
2024-09-15 20:21:06 -0600

    update login function (include id)

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,41 +1,39 @@
-const express = require('express');
-const createError = require('http-errors'); 
+const express = require("express");
+const createError = require("http-errors");
 
-const userUseCases = require('../usecases/user.usecases');
+const userUseCases = require("../usecases/user.usecases");
 
 const router = express.Router();
 
-
 // Login example localhost:port/user/login
 router.post("/login", async (request, response) => {
-    try {
-      const data = request.body;
-      const token = await userUseCases.login(data);
-  
-      response.json({
-        success: true,
-        message: "User logged in",
-        data: { token },
-      });
-    } catch (error) {
-      response.status(error.status || 500);
-      response.json({
-        success: false,
-        message: error.message,
-      });
-    }
-  });
-  
+  try {
+    const data = request.body;
+    const token = await userUseCases.login(data);
+
+    response.json({
+      success: true,
+      message: "User logged in",
+      data: { id, token },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // Signup route localhost:port/signup
-router.post('/signup', async (request, response) => {
+router.post("/signup", async (request, response) => {
   try {
     const userData = request.body;
     const newUser = await userUseCases.signUp(userData);
 
     response.json({
       success: true,
-      message: 'User created',
+      message: "User created",
       data: { user: newUser },
     });
   } catch (error) {
@@ -48,20 +46,19 @@ router.post('/signup', async (request, response) => {
 });
 
 //Get user information for Id
-router.get('/:id', async (request, response) => {
+router.get("/:id", async (request, response) => {
   try {
     const id = request.params.id;
     const user = await userUseCases.getById(id);
 
     if (!user) {
-      throw createError(404, 'User not found');
+      throw createError(404, "User not found");
     }
     response.json({
       success: true,
-      message: 'User found by Id',
+      message: "User found by Id",
       data: { user },
     });
-
   } catch (error) {
     response.status(error.status || 500);
     response.json({
@@ -70,6 +67,5 @@ router.get('/:id', async (request, response) => {
     });
   }
 });
-
 
 module.exports = router;
--- src/usecases/user.usecases.js
+++ src/usecases/user.usecases.js
@@ -1,102 +1,100 @@
-const createError = require('http-errors');
+const createError = require("http-errors");
 
-const encryption = require('../lib/encryption');
-const jwt = require('../lib/jwt');
+const encryption = require("../lib/encryption");
+const jwt = require("../lib/jwt");
 
 const User = require("../models/user.model");
 
 // Login function for user
 async function login(data) {
-    const user = await User.findOne( {email: data.email} ).select('+password');
+  const user = await User.findOne({ email: data.email }).select("+password");
 
-    if (!user) {
-        throw createError(401, 'User not found 🫤');
-    }
+  if (!user) {
+    throw createError(401, "User not found 🫤");
+  }
 
-    const isValidPassword = encryption.compare(data.password, user.password);
+  const isValidPassword = encryption.compare(data.password, user.password);
 
-    if (!isValidPassword) {
-        throw createError(401, 'Invalid credentials 🫤');
-    }
+  if (!isValidPassword) {
+    throw createError(401, "Invalid credentials 🫤");
+  }
 
-    const token = jwt.sign( {id: user._id} );
+  const token = jwt.sign({ id: user._id });
 
-    return {token, user}; // Mostrar token y datos de user
-
+  return { token, user, id: user._id }; // Mostrar token y datos de user
 }
 
 // Signup function for user
 
 async function signUp(data) {
-    const userFound = await User.findOne({ email: data.email });
+  const userFound = await User.findOne({ email: data.email });
 
-    if (userFound) {
-        throw createError(409, 'Email already registered');
-    }
+  if (userFound) {
+    throw createError(409, "Email already registered");
+  }
 
-    if (!data.password) {
-        throw createError(400, 'Password is required');
-    }
+  if (!data.password) {
+    throw createError(400, "Password is required");
+  }
 
-    if (data.password.length < 6) {
-        throw createError(400, 'Password must be at least 6 characters long');
-    }
+  if (data.password.length < 6) {
+    throw createError(400, "Password must be at least 6 characters long");
+  }
 
-    const password = encryption.encrypt(data.password) 
+  const password = encryption.encrypt(data.password);
 
-    data.password = password;
-    
-    const newUser = await User.create(data);
-    
-    return newUser;
+  data.password = password;
+
+  const newUser = await User.create(data);
+
+  return newUser;
 }
 
 // Create a new user
 async function create(data) {
-    const user = await User.create(data);
-    return user;
+  const user = await User.create(data);
+  return user;
 }
 
 // Get all Users
 async function getAllUsers() {
-    const users = await User.find({});
-    return users;
+  const users = await User.find({});
+  return users;
 }
 
 // Get User by ID
 async function getById(id) {
-    const user = await User.findById(id);
-    return user;
+  const user = await User.findById(id);
+  return user;
 }
 
 // Update User by ID
 async function updateUserById(id, newData) {
-    const userFound = await User.findById(id);
+  const userFound = await User.findById(id);
 
-    if (!userFound) {
-        throw createError(404, 'User not found 🫤');
-    }
+  if (!userFound) {
+    throw createError(404, "User not found 🫤");
+  }
 
-    const userUpdated = await User.findByIdAndUpdate(id, newData, { new: true });
-    return userUpdated;
+  const userUpdated = await User.findByIdAndUpdate(id, newData, { new: true });
+  return userUpdated;
 }
 
 // Delete User by ID
 async function deleteUserById(id) {
-    const userFound = await User.findById(id);
+  const userFound = await User.findById(id);
 
-    if (!userFound) {
-        throw createError(404, 'User not found 🫤');
-    }
-    
+  if (!userFound) {
+    throw createError(404, "User not found 🫤");
+  }
 }
 
 module.exports = {
-    login,
-    signUp,
-    create,
-    getAllUsers,
-    getById,
-    updateUserById,
-    deleteUserById
-}
+  login,
+  signUp,
+  create,
+  getAllUsers,
+  getById,
+  updateUserById,
+  deleteUserById,
+};
