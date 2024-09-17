2385adab53ec6ce5edc0e53581ed8cd1ee84fe52
Jairo B <jairock@outlook.com>
2024-09-16 21:46:05 -0600

    update post model (include hashtags)

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,25 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
+// const postSchema = new mongoose.Schema({
+//     title: {
+//         type: 'string',
+//         required: true,
+//         maxlength: 120,
+//         minlength: 2
+//     },
+//     image: {
+//         type: 'string',
+//         default: 'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg'
+//     },
+//     body: {
+//         type: 'string',
+//         required: true,
+//         minlength: 20
+//     },
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'user',
+//     }
+// },{timestamps: true});
+
+const postSchema = new mongoose.Schema(
+  {
     title: {
-        type: 'string',
-        required: true,
-        maxlength: 120,
-        minlength: 2
+      type: "string",
+      required: true,
+      maxlength: 120,
+      minlength: 2,
     },
     image: {
-        type: 'string',
-        default: 'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg'
+      type: "string",
+      default:
+        "https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress&fm=pjpg",
     },
     body: {
-        type: 'string',
-        required: true,
-        minlength: 20
+      type: "string",
+      required: true,
+      minlength: 20,
     },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
-    }
-},{timestamps: true});
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    hashtags: {
+      type: [String],
+      validate: {
+        validator: function (arr) {
+          return arr.length <= 4;
+        },
+        message: "You can add up to 4 hashtags.",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
